68dfdc4a827ffdcc0662e0dab5f0d5e88fb45ff4
dsuccar <danielsuccar@icloud.com>
2026-05-28 13:05:11 -0400

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,21 @@
-import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react";
-
+import { Button } from '@/components/ui/button';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
-   
-  useEffect(() => {
-    fetch(`/api/hello`)
-    .then(res => res.json())
-    .then(data => setMessage(data.message))
-  }, []);
+   const [message, setMessage] = useState('');
 
-  return (
-  <div className="p-4">
-    <p className="font-bold text-3xl">{message}</p>
-    <Button> Click Me</Button>
-  </div>
-  )
-  
-  
+   useEffect(() => {
+      fetch(`/api/hello`)
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button> Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,22 @@
-import express from "express";
+import express from 'express';
 import type { Request, Response } from 'express';
-import { LanguageServiceMode } from "typescript";
+import { LanguageServiceMode } from 'typescript';
 import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const port =  process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
-    // process.env.OPENAI_API_KEY
+   res.send('Hello World');
+   // process.env.OPENAI_API_KEY
 });
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello World'})
-    // process.env.OPENAI_API_KEY
+   res.json({ message: 'Hello World' });
+   // process.env.OPENAI_API_KEY
 });
 
 app.listen(port, () => {
-    console.log (`Server is  running on http://localhost:${port}`)
+   console.log(`Server is  running on http://localhost:${port}`);
 });
